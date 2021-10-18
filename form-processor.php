86493f2cda0b330eb4044353d72a1fff5f559f19
iamlogan <iamlogandavidson@gmail.com>
2021-10-18 22:19:08 +0100

    Form to Email setup!

--- form-processor.php
+++ form-processor.php
@@ -7,16 +7,15 @@
     $message = $_POST['message'];
 
     $email_from = 'website@carlislestudentproperties.co.uk';
-	$email_subject = "New Arrange Viewing Request";
-	$email_body = "Name: $name.\n".
-	    "Email: $visitor_email\n".
-	    "Phone: $phone\n".
-	    "Properties: $properties\n".
-	    "Group size: $group\n".
+	$email_subject = "Online Viewing Request";
+	$email_body = "Name:  $name\n\n".
+	    "Email:  $visitor_email\n\n".
+	    "Phone:  $phone\n\n".
+	    "Properties:  $properties\n\n".
+	    "Group size:  $group\n\n".
         "Message:\n$message\n";
 
-
-    $to = "iamlogandavidson@gmail.com";
+    $to = "carlislestudentproperties@aol.co.uk";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
     mail($to,$email_subject,$email_body,$headers);
