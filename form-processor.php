4828d15e8d3464adcf026edee4f8402a9bdd1b19
iamlogan <iamlogandavidson@gmail.com>
2021-10-22 16:18:44 +0100

    Email production environment testing

--- form-processor.php
+++ form-processor.php
@@ -16,7 +16,7 @@
         "Message:\n$message\n";
 
 // Send email to carlislestudentproperties@aol.co.uk
-    $to = "carlislestudentproperties@aol.co.uk";
+    $to = "carlislestudentproperties@aol.co.uk, carlislestudentproperties@gmail.com";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
     mail($to,$email_subject,$email_body,$headers);
